72cafbe582c420afc27ccea6196463bcf9107167
Austin Schrader <austin.jschrader@gmail.com>
2024-11-11 12:29:38 -0800

    Add shadcn and base CityRankingMap

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import CityRankingMap from "./CityRankingMap";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <h1 className="text-4xl text-blue-500 underline">Hello</h1>;
+  return (
+    <>
+      <h1 className="text-4xl text-blue-500 underline">Hello</h1>
+      <CityRankingMap />
+    </>
+  );
 }
 
 export default App;
