56c3fb3f3c00a790ad434cf3acdc048fae668bd3
Austin Schrader <austin.jschrader@gmail.com>
2024-11-19 14:34:33 -0800

    Add proxy configuration for API rewrites in Vite

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-        },
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/data": {
+        target: "https://20.3.188.93",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/data/, "/api"),
+      },
     },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
