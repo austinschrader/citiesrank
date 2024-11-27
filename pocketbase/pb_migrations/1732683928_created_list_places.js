06693425ca32f60072bb67774cc6a96c4652f17a
Austin Schrader <austin.jschrader@gmail.com>
2024-11-26 21:09:47 -0800

    Add new collections for list tags, list places, related lists, and list stats with updated schema

---
+++ pocketbase/pb_migrations/1732683928_created_list_places.js
@@ -0,0 +1,77 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const collection = new Collection({
+    "id": "list_places_table",
+    "created": "2024-11-27 05:05:28.009Z",
+    "updated": "2024-11-27 05:05:28.009Z",
+    "name": "list_places",
+    "type": "base",
+    "system": false,
+    "schema": [
+      {
+        "system": false,
+        "id": "list_places_list_id",
+        "name": "list",
+        "type": "relation",
+        "required": true,
+        "presentable": false,
+        "unique": false,
+        "options": {
+          "collectionId": "lists_table",
+          "cascadeDelete": true,
+          "minSelect": null,
+          "maxSelect": 1,
+          "displayFields": [
+            "title"
+          ]
+        }
+      },
+      {
+        "system": false,
+        "id": "list_places_city_id",
+        "name": "city",
+        "type": "relation",
+        "required": true,
+        "presentable": false,
+        "unique": false,
+        "options": {
+          "collectionId": "cities_list",
+          "cascadeDelete": false,
+          "minSelect": null,
+          "maxSelect": 1,
+          "displayFields": [
+            "name"
+          ]
+        }
+      },
+      {
+        "system": false,
+        "id": "list_places_order",
+        "name": "order",
+        "type": "number",
+        "required": true,
+        "presentable": false,
+        "unique": false,
+        "options": {
+          "min": 0,
+          "max": null,
+          "noDecimal": true
+        }
+      }
+    ],
+    "indexes": [],
+    "listRule": "",
+    "viewRule": "",
+    "createRule": "",
+    "updateRule": "",
+    "deleteRule": "",
+    "options": {}
+  });
+
+  return Dao(db).saveCollection(collection);
+}, (db) => {
+  const dao = new Dao(db);
+  const collection = dao.findCollectionByNameOrId("list_places_table");
+
+  return dao.deleteCollection(collection);
+})
