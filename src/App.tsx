3aa693075b728aefbe891392f41ea29af0e1d50e
Austin Schrader <austin.jschrader@gmail.com>
2024-11-29 22:54:14 -0800

    Organize pages into subfolders

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import "./App.css";
-import { PlacesPage } from "@/pages/PlacesPage";
-import { ListsPage } from "@/pages/ListsPage";
+import { PlacesPage } from "@/pages/places/PlacesPage";
+import { ListsPage } from "@/pages/lists/ListsPage";
 import { MembersPage } from "@/pages/MembersPage";
 import { JournalPage } from "@/pages/JournalPage";
 import { SavedPage } from "@/pages/SavedPage";
-import { AddPlacePage } from "@/pages/AddPlacePage";
+import { AddPlacePage } from "@/pages/places/AddPlacePage";
 import { Routes, Route } from "react-router-dom";
 import { RootLayout } from "@/layouts/RootLayout";
-import { CreateListPage } from "@/pages/CreateListPage";
-import { ViewListPage } from "@/pages/ViewListPage";
+import { CreateListPage } from "@/pages/lists/CreateListPage";
+import { ViewListPage } from "@/pages/lists/ViewListPage";
 import { AuthProvider } from "@/features/auth/context/AuthContext";
 import { SettingsPage } from "@/pages/SettingsPage"; // You'll need to create this
 import { ProfilePage } from "@/pages/ProfilePage"; // You'll need to create this
 import { Toaster } from "@/components/ui/toaster"; // Add this import
-import { CityDetailsPage } from "@/pages/CityDetailsPage"; // Add this import
+import { CityDetailsPage } from "@/pages/places/CityDetailsPage"; // Add this import
 import { PreferencesProvider } from "@/features/preferences/context/PreferencesContext";
 
 function App() {
