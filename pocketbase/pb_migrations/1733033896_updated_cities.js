05acde19e8dd26ab822a0133ba7ad18bb6c55b41
Austin Schrader <austin.jschrader@gmail.com>
2024-12-01 01:10:07 -0800

    Add tags to db

---
+++ pocketbase/pb_migrations/1733033896_updated_cities.js
@@ -0,0 +1,29 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("cities_list")
+
+  // remove
+  collection.schema.removeField("city_reviews_json_data")
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("cities_list")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "city_reviews_json_data",
+    "name": "reviews",
+    "type": "json",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "maxSize": 2000000
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
---
+++ pocketbase/pb_migrations/1733043306_updated_cities.js
@@ -0,0 +1,29 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("cities_list")
+
+  // remove
+  collection.schema.removeField("city_destination_types_list")
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("cities_list")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "city_destination_types_list",
+    "name": "destinationTypes",
+    "type": "json",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "maxSize": 2000000
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
