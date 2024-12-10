177e6bf6bff9b95a626a24ed0d99c655877cdf3a
Austin Schrader <austin.jschrader@gmail.com>
2024-12-10 02:36:56 -0800

    Remove unused settings page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Toaster } from "@/components/ui/toaster"; // Add this import
+import { Toaster } from "@/components/ui/toaster";
 import { AuthProvider } from "@/features/auth/context/AuthContext";
 import { CitiesProvider } from "@/features/places/context/CitiesContext";
 import { CountriesProvider } from "@/features/places/context/CountriesContext";
@@ -6,10 +6,9 @@ import { PreferencesProvider } from "@/features/preferences/context/PreferencesC
 import { RootLayout } from "@/layouts/RootLayout";
 import "@/lib/styles/App.css";
 import FavoritesPage from "@/pages/favorites/FavoritesPage";
-import { PlaceDetailsPage } from "@/pages/places/PlaceDetailsPage"; // Add this import
+import { PlaceDetailsPage } from "@/pages/places/PlaceDetailsPage";
 import { PlacesPage } from "@/pages/places/PlacesPage";
-import { ProfilePage } from "@/pages/profile/ProfilePage"; // You'll need to create this
-import { SettingsPage } from "@/pages/settings/SettingsPage"; // You'll need to create this
+import { ProfilePage } from "@/pages/profile/ProfilePage";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
@@ -22,7 +21,6 @@ function App() {
               <Routes>
                 <Route path="/" element={<PlacesPage />} />
                 <Route path="/favorites" element={<FavoritesPage />} />
-                <Route path="/settings" element={<SettingsPage />} />
                 <Route path="/profile" element={<ProfilePage />} />
                 <Route
                   path="/places/:placeType/:id"
