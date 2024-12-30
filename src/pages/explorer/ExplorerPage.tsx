6da7b9dfc055b620f771a7205ede8afb7ade1f17
Austin Schrader <austin.jschrader@gmail.com>
2024-12-30 00:42:08 -0800

    Fix scrolling bug

--- src/pages/explorer/ExplorerPage.tsx
+++ src/pages/explorer/ExplorerPage.tsx
@@ -2,7 +2,7 @@ import { SplitExplorer } from "@/features/explorer/components/SplitExplorer";
 
 export const ExplorerPage = () => {
   return (
-    <div className="h-[calc(100vh-4rem)] w-full">
+    <div className="h-[calc(100vh-8rem)] w-full">
       <SplitExplorer />
     </div>
   );
